76375de2a954958751da392a02c868cb37a6128a
AhmedBabaAlweimine <ahmedouldbaba4@gmail.com>
2022-03-12 14:20:39 +0100

    oui

--- src/main/Bonjour.java
+++ src/main/Bonjour.java
@@ -2,6 +2,9 @@ package main;
 
 public class Bonjour {
 public static void main(String[] argv){
+System.out.println("Take it all");	
+System.out.println("bien");	
 System.out.println("Bonjour khoya sidi");	
+
 }
 }
