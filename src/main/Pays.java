8e52002096f8f793d0072a4498c7acdb657c1ab6
AhmedBabaAlweimine <ahmedouldbaba4@gmail.com>
2022-03-12 21:24:11 +0100

    bug 6754: fixer bug constructor et setter et getter

--- src/main/Pays.java
+++ src/main/Pays.java
@@ -7,13 +7,29 @@ private Long uid;
 private String nom;
 private String prenom;
 private List<Bonjour> greeting;
-public Pays(Long uid, String nom, String prenom, List<Pays> paysvisites) {
+
+public Pays(Long uid, String nom, String prenom, List<Bonjour> greeting, List<Pays> paysvisites) {
 	super();
 	this.uid = uid;
 	this.nom = nom;
 	this.prenom = prenom;
+	this.greeting = greeting;
 	this.paysvisites = paysvisites;
 }
+
+public String getPrenom() {
+	return prenom;
+}
+public List<Bonjour> getGreeting() {
+	return greeting;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public void setGreeting(List<Bonjour> greeting) {
+	this.greeting = greeting;
+}
+
 private List<Pays> paysvisites;
 public Long getUid() {
 	return uid;
