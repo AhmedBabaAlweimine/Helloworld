143e808b19a776dd4cd1db2611c162d331f49e3a
AhmedBabaAlweimine <ahmedouldbaba4@gmail.com>
2022-03-12 20:42:17 +0100

    SGIId-89765: generation de setter et getter

--- src/main/Pays.java
+++ src/main/Pays.java
@@ -1,6 +1,27 @@
 package main;
 
+import java.util.List;
+
 public class Pays {
 private Long uid;	
 private String nom;
+private List<Pays> paysvisites;
+public Long getUid() {
+	return uid;
+}
+public String getNom() {
+	return nom;
+}
+public List<Pays> getPaysvisites() {
+	return paysvisites;
+}
+public void setUid(Long uid) {
+	this.uid = uid;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public void setPaysvisites(List<Pays> paysvisites) {
+	this.paysvisites = paysvisites;
+}
 }
