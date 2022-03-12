b523c2c57bd0e53ce510e9ccc3a5d8b3363d75e7
AhmedBabaAlweimine <ahmedouldbaba4@gmail.com>
2022-03-12 21:03:47 +0100

    bug2788: generation constructor

--- src/main/Pays.java
+++ src/main/Pays.java
@@ -6,6 +6,14 @@ public class Pays {
 private Long uid;	
 private String nom;
 private String prenom;
+
+public Pays(Long uid, String nom, String prenom, List<Pays> paysvisites) {
+	super();
+	this.uid = uid;
+	this.nom = nom;
+	this.prenom = prenom;
+	this.paysvisites = paysvisites;
+}
 private List<Pays> paysvisites;
 public Long getUid() {
 	return uid;
